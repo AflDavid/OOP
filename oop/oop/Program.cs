cd8d2cbd4b317dac28a4d0f00f75133bc4fd9ab5
AflDavid <93366100+AflDavid@users.noreply.github.com>
2021-12-09 21:51:30 +0200

    Add files via upload

---
+++ oop/oop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            int n;
+            n = Int32.Parse(Console.ReadLine());
+            int[] vector = arrayHelper.Fibonacci(n);
+
+            for (int i = 0; i < vector.Length; i++)
+                Console.WriteLine(vector [i]+ " ");
+
+            Console.WriteLine();
+
+            int nr = Int32.Parse(Console.ReadLine());
+            int[] vector1 = new int[nr];
+
+            for (int i = 0; i < nr; i++)
+            {
+                Console.WriteLine($"vector1[{i}]");
+                vector1[i] = Int32.Parse(Console.ReadLine());
+
+            }
+            int element = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(arrayHelper.Frecventa(vector1, element));
+            Console.WriteLine("Introduceti numarul de linii din matrice!");
+
+            int n1 = Int32.Parse(Console.ReadLine());
+            int[,] matrix = new int[n1, n1];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"matrix[{row},{col}]:");
+                    matrix[row, col] = Int32.Parse(Console.ReadLine());
+
+                }
+            }
+            if (arrayHelper.Identitate(matrix) == true)
+                Console.WriteLine("Este matrice identitate!");
+            else
+                Console.WriteLine("Nu este matrice identitate!");
+        }
+    }
+}
